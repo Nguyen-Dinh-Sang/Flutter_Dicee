0e35054d09182d260fde0ba2c91479a31bb7eec1
SSG\sangnd <sangnd@ssg.vn>
2021-06-21 14:41:31 +0700

    Dicee-DinhSang: inwell

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -18,6 +19,33 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16, 16, 8, 16),
+              child: InkWell(
+                child: Image.asset('images/dice1.png'),
+                onTap: () {
+                  print('left tap');
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(8, 16, 16, 16),
+              child: InkWell(
+                child: Image.asset('images/dice3.png'),
+                onTap: () {
+                  print('right tap');
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
