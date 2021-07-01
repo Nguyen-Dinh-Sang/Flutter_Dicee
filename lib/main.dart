7875af55f1be25cf464b86b3a1d6c04f0e1e79da
SSG\sangnd <sangnd@ssg.vn>
2021-07-01 11:58:37 +0700

    Dicee-DinhSang: random

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   return runApp(
@@ -22,40 +25,57 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  var letDiceNumber = 4;
+  var letDiceNumber = 6;
   var rightDiceNumber = 6;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(16, 16, 8, 16),
-              child: InkWell(
-                child: Image.asset('images/dice$letDiceNumber.png'),
-                onTap: () {
-                  setState(() {
-                    letDiceNumber = 1;
-                  });
-                },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16, 16, 8, 16),
+                child: InkWell(
+                  child: Image.asset('images/dice$letDiceNumber.png'),
+                  onTap: () {
+                    setState(() {
+                      ranDom();
+                    });
+                  },
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(8, 16, 16, 16),
-              child: InkWell(
-                child: Image.asset('images/dice$rightDiceNumber.png'),
-                onTap: () {
-                  print('right tap');
-                },
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(8, 16, 16, 16),
+                child: InkWell(
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                  onTap: () {
+                    ranDom();
+                    setState(() {});
+                  },
+                ),
               ),
             ),
+          ],
+        ),
+        Text(
+          '${letDiceNumber + rightDiceNumber}',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 50,
           ),
-        ],
-      ),
+        ),
+      ],
     );
+  }
+
+  void ranDom() {
+    letDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
   }
 }
